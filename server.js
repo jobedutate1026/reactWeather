d2b3bdbedd1bb5aa1521677731b6e8fd802943c7
jobedutate1026 <oned.utate@gmail.com>
2016-10-23 00:04:35 -0400

    updated some files

--- server.js
+++ server.js
@@ -18,3 +18,14 @@ app.use(express.static('public'));
 app.listen(PORT, function(){
   console.log("Express server is up on port " + PORT);
 });
+
+// var express = require('express');
+//
+// // Create our app
+// var app = express();
+//
+// app.use(express.static('public'));
+//
+// app.listen(3000, function () {
+//   console.log('Express server is up on port 3000');
+// });
